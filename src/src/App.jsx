0c199d64a7c4a253f252c06b92b014268d3283f0
Sai Raveendra Kandregula <kandregulas@iitbhilai.ac.in>
2022-09-14 11:56:29 +0000

    Integrated List of Users Page

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -6,9 +6,15 @@ import ECIIcon from './assets/eci_logo.png';
 import {ReactComponent as HomeIcon} from './assets/Home.svg';
 import {ReactComponent as UserManagementIcon} from './assets/Users.svg';
 import {ReactComponent as OrderManagementIcon} from './assets/Order.svg';
+import {ReactComponent as UnitManagementIcon} from './assets/UnitManagement.svg';
+import {ReactComponent as WarehouseManagementIcon} from './assets/WarehouseManagement.svg';
+import {ReactComponent as IssueRequestManagementIcon} from './assets/Issue-RequestManagement.svg';
+import {ReactComponent as OtherServicesIcon} from './assets/OtherServices.svg';
+import {ReactComponent as ConsumableManagementIcon} from './assets/ConsumableManagement.svg';
 import {ReactComponent as LogoutIcon} from './assets/Logout.svg';
 import CreateUser from './pages/user_management/CreateUser';
 import Home from './pages/home/Home';
+import ManageUsers from './pages/user_management/ManageUsers';
 
 function App() {
 	return (
@@ -27,12 +33,27 @@ function App() {
 						 	<HomeIcon />Home
 						</button>
 						<button className='nav-button' onClick={()=>{
-							window.location.href = "/usermanagement/createuser" 
+							window.location.href = "/usermanagement" 
 						}}>
 						 	<UserManagementIcon />User Management
 						</button>
 						<button className='nav-button'>
 						 	<OrderManagementIcon />Order Management
+						</button>
+						<button className='nav-button'>
+						 	<UnitManagementIcon />Unit Management
+						</button>
+						<button className='nav-button'>
+						 	<WarehouseManagementIcon />Warehouse Management
+						</button>
+						<button className='nav-button'>
+						 	<IssueRequestManagementIcon />Issue/Request Management
+						</button>
+						<button className='nav-button'>
+						 	<OtherServicesIcon />Other Services
+						</button>
+						<button className='nav-button'>
+						 	<ConsumableManagementIcon />Consumable Management
 						</button>
 					</div>
 					<div className="nav-panel-bottom">
@@ -46,6 +67,7 @@ function App() {
 						<BrowserRouter>
 							<Routes>
 								<Route path="/" element={<Home />} /> {/* Dashboard Component goes here */}
+								<Route path="/usermanagement/" element={<ManageUsers />} />
 								<Route path="/usermanagement/createuser" element={<CreateUser />} />
 							</Routes>
 						</BrowserRouter>
