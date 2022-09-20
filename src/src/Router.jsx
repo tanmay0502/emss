1301b6826fa5709f1c12770e740fb1a1e936f4ec
Sai Raveendra Kandregula <kandregulas@iitbhilai.ac.in>
2022-09-20 16:52:35 +0000

    Updated Create Warehouse Page Layout

--- src/src/Router.jsx
+++ src/src/Router.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import ECIIcon from './assets/eci_logo.png';
-import {ReactComponent as HomeIcon} from './assets/Home.svg';
-import {ReactComponent as UserManagementIcon} from './assets/Users.svg';
-import {ReactComponent as OrderManagementIcon} from './assets/Order.svg';
-import {ReactComponent as UnitManagementIcon} from './assets/UnitManagement.svg';
-import {ReactComponent as WarehouseManagementIcon} from './assets/WarehouseManagement.svg';
-import {ReactComponent as IssueRequestManagementIcon} from './assets/Issue-RequestManagement.svg';
-import {ReactComponent as OtherServicesIcon} from './assets/OtherServices.svg';
-import {ReactComponent as ConsumableManagementIcon} from './assets/ConsumableManagement.svg';
-import {ReactComponent as LogoutIcon} from './assets/Logout.svg';
+import { ReactComponent as HomeIcon } from './assets/Home.svg';
+import { ReactComponent as UserManagementIcon } from './assets/Users.svg';
+import { ReactComponent as OrderManagementIcon } from './assets/Order.svg';
+import { ReactComponent as UnitManagementIcon } from './assets/UnitManagement.svg';
+import { ReactComponent as WarehouseManagementIcon } from './assets/WarehouseManagement.svg';
+import { ReactComponent as IssueRequestManagementIcon } from './assets/Issue-RequestManagement.svg';
+import { ReactComponent as OtherServicesIcon } from './assets/OtherServices.svg';
+import { ReactComponent as ConsumableManagementIcon } from './assets/ConsumableManagement.svg';
+import { ReactComponent as LogoutIcon } from './assets/Logout.svg';
 import { useEffect } from 'react';
 import { useState } from 'react';
 
@@ -19,89 +19,93 @@ function Routed(props) {
 		username: null
 	})
 
-	const fetchUserData = async (userid) =>{
+	const fetchUserData = async (userid) => {
 		const response = await fetch(
 			`http://evm.iitbhilai.ac.in:8000/getNameFromUserID/${userid}`,
 			{
-			  method: "GET",
-			  headers: {
-				"Content-Type": "application/json",
-			  },
-	
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+				},
+
 			}
-		  );
-		  return await response.json().then(val => val.name);
+		);
+		return await response.json().then(val => val.name);
 	}
 
 	useEffect(() => {
 
 
-	  if( sessionStorage.getItem('sessionToken') === null ){
-		window.location.href = '/login'
-	  }
+		if (sessionStorage.getItem('sessionToken') === null) {
+			window.location.href = '/login'
+		}
 
-	  fetchUserData(sessionStorage.getItem('sessionToken')).then((val)=> {
-		setUserData({
-			userId: sessionStorage.getItem('sessionToken'),
-			username: val
+		fetchUserData(sessionStorage.getItem('sessionToken')).then((val) => {
+			setUserData({
+				userId: sessionStorage.getItem('sessionToken'),
+				username: val
+			})
 		})
-	  })
-	  
-	
-	  return () => {
-		
-	  }
-	}, [])
-	
 
-  return (
-	<div className="App">
-			<header className="App-header">
+
+		return () => {
+
+		}
+	}, [])
+
+
+	return (
+		<div className="App">
+			{/* <header className="App-header">
 				<img className='App-Icon' src={ECIIcon} />
 				EVM Management System
-			</header>
+			</header> */}
 			<main>
 				<div className="nav-panel">
 					<div className="nav-panel-top">
+						<div className='app-info'>
+							<img className='App-Icon' src={ECIIcon} />
+							EVM Management System
+						</div>
 						<span>Logged in as: {userData.userId}</span>
 						<span>Welcome, {userData.username}</span>
-						<button className='nav-button' onClick={()=>{
-							window.location.href = "/session/home" 
+						<button className='nav-button' onClick={() => {
+							window.location.href = "/session/home"
 						}}>
-						 	<HomeIcon />Home
+							<HomeIcon />Home
 						</button>
-						<button className='nav-button' onClick={()=>{
-							window.location.href = "/session/usermanagement" 
+						<button className='nav-button' onClick={() => {
+							window.location.href = "/session/usermanagement"
 						}}>
-						 	<UserManagementIcon />User Management
+							<UserManagementIcon />User Management
 						</button>
 						<button className='nav-button'>
-						 	<OrderManagementIcon />Order Management
+							<OrderManagementIcon />Order Management
 						</button>
 						<button className='nav-button'>
-						 	<UnitManagementIcon />Unit Management
+							<UnitManagementIcon />Unit Management
 						</button>
-						<button className='nav-button' onClick={()=>{
-							window.location.href = "/session/warehousemanagement" 
+						<button className='nav-button' onClick={() => {
+							window.location.href = "/session/warehousemanagement"
 						}}>
-						 	<WarehouseManagementIcon />Warehouse Management
+							<WarehouseManagementIcon />Warehouse Management
 						</button>
 						<button className='nav-button'>
-						 	<IssueRequestManagementIcon />Issue/Request Management
+							<IssueRequestManagementIcon />Issue/Request Management
 						</button>
 						<button className='nav-button'>
-						 	<OtherServicesIcon />Other Services
+							<OtherServicesIcon />Other Services
 						</button>
 						<button className='nav-button'>
-						 	<ConsumableManagementIcon />Consumable Management
+							<ConsumableManagementIcon />Consumable Management
 						</button>
 					</div>
 					<div className="nav-panel-bottom">
-						<button className='nav-button' onClick={()=>{
+						<button className='nav-button' onClick={() => {
 							sessionStorage.removeItem('sessionToken')
 							window.location.href = '/'
 						}}>
-						 	<LogoutIcon />Logout
+							<LogoutIcon />Logout
 						</button>
 					</div>
 				</div>
@@ -112,7 +116,7 @@ function Routed(props) {
 				</div>
 			</main>
 		</div>
-  )
+	)
 }
 
 export default Routed
--- src/src/pages/warehouse_management/AddWarehouse.jsx
+++ src/src/pages/warehouse_management/AddWarehouse.jsx
@@ -12,10 +12,13 @@ import { BsFillPersonFill } from 'react-icons/bs'
 import { BsFillTelephoneFill } from 'react-icons/bs'
 import { MdWork } from "react-icons/md";
 import { BsCheck2 } from 'react-icons/bs'
-
+import { useState } from "react";
 
 
 export default function AddWarehouse() {
+
+	const [doubleLockSystem, setDoubleLockSystem] = useState(true)
+
 	return (
 		<div className="flex-col justify-center align-middle">
 			<div className="content-path">
@@ -27,220 +30,236 @@ export default function AddWarehouse() {
 					<h4><FaWarehouse /><span>Add New Warehouse</span></h4>
 				</div>
 				<form className="myForm">
-					<h5>
-						Warehouse Details
-					</h5>
-					<div className="input_group three-column-grid">
+					<div className="formGrid">
+						<div class="warehouse-type">
+							<h5>
+								Warehouse Type
+							</h5>
+							<div className="input_group">
+								<div className="form_group">
+									<div className="form_label">
+										<label htmlFor="">Type : </label>
+									</div>
+									<div className="form_select">
+										<select name="" id="">
+											<option value="-1" className="FirstOption">--Select--</option>
+											<option value="01">National Reserve Warehouse</option>
+											<option value="02">State Central Warehouse</option>
+											<option value="03">District Warehouse</option>
+											<option value="04">District Strong Room</option>
 
-						<div className="form_group">
-							<div className="form_label">
-								<label htmlFor="">Type : </label>
-							</div>
-							<div className="form_select">
-								<select name="" id="">
-									<option value="-1" className="FirstOption">--Select--</option>
-									<option value="01">National Reserve Warehouse</option>
-									<option value="02">State Central Warehouse</option>
-									<option value="03">District Warehouse</option>
-									<option value="04">District Strong Room</option>
+											<option value="05">Strong Room for AC</option>
+											<option value="06">Strong Room for AS (PC)</option>
+											<option value="07">Strong Room for Polled Units</option>
+											<option value="08">Strong Room for Units under Counting</option>
 
-									<option value="05">Strong Room for AC</option>
-									<option value="06">Strong Room for AS (PC)</option>
-									<option value="07">Strong Room for Polled Units</option>
-									<option value="08">Strong Room for Units under Counting</option>
+											<option value="09">Strong Room for units under EP Period</option>
+											<option value="10">Strong Room for EP Marked units</option>
+											<option value="11">Defective EVM Warehouse</option>
+											<option value="12">Reserve Units Strong Room</option>
 
-									<option value="09">Strong Room for units under EP Period</option>
-									<option value="10">Strong Room for EP Marked units</option>
-									<option value="11">Defective EVM Warehouse</option>
-									<option value="12">Reserve Units Strong Room</option>
+											<option value="13">T&A Warehouse</option>
+											<option value="14">Virtual Warehouse</option>
+											<option value="15">Virtual Strong Room</option>
+											<option value="16">Manufacturer Warehouse</option>
+										</select>
+										<div className="input_icon">
+											<FaWarehouse size="1em" />
+										</div>
+									</div>
 
-									<option value="13">T&A Warehouse</option>
-									<option value="14">Virtual Warehouse</option>
-									<option value="15">Virtual Strong Room</option>
-									<option value="16">Manufacturer Warehouse</option>
-								</select>
-								<div className="input_icon">
-									<FaWarehouse size="1em" />
 								</div>
-							</div>
 
+								<div className="form_group">
+									<div className="form_label">
+										<label htmlFor="">Building Type : </label>
+									</div>
+									<div className="form_select">
+										<select name="" id="">
+											<option value="-1" className="FirstOption">--Select--</option>
+
+											<option value="Temporary">Temporary</option>
+											<option value="Permenant">Permanent</option>
+										</select>
+										<div className="input_icon">
+											<FaRegBuilding size="1em" />
+										</div>
+									</div>
+								</div>
+
+								<div className="form_group">
+									<div className="form_label">
+										<label htmlFor="">Sealed: </label>
+									</div>
+									<div className="form_select">
+										<select name="" id="">
+											<option value="-1" className="FirstOption">--Select--</option>
+											<option value="Yes">Yes</option>
+											<option value="No">No</option>
+										</select>
+										<div className="input_icon">
+											<BsShieldLockFill size="1em" />
+										</div>
+									</div>
+
+								</div> </div>
 						</div>
+						<div class="warehouse-location">
+							<h5>
+								Warehouse Location
+							</h5>
+							<div className="input_group">
+								<div className="form_group">
+									<div className="form_label">
+										<label htmlFor="">Address : </label>
+									</div>
+									<div className="form_input">
+										<input id="" name="" className="" placeholder="Warehouse Address" />
+										<div className="input_icon">
+											<FaMapMarkedAlt size="1em" />
+										</div>
+									</div>
 
-						<div className="form_group">
-							<div className="form_label">
-								<label htmlFor="">Building Type : </label>
-							</div>
-							<div className="form_select">
-								<select name="" id="">
-									<option value="-1" className="FirstOption">--Select--</option>
+								</div>
 
-									<option value="Temporary">Temporary</option>
-									<option value="Permenant">Permanent</option>
-								</select>
-								<div className="input_icon">
-									<FaRegBuilding size="1em" />
+								<div className="form_group">
+									<div className="form_label">
+										<label htmlFor="">Warehouse Code : </label>
+									</div>
+									<div className="form_input">
+										<input id="" name="" className="" placeholder="Warehouse Code" />
+										<div className="input_icon">
+											<FaLaptopHouse size="1em" />
+										</div>
+									</div>
+								</div>
+
+								<div className="form_group">
+									<div className="form_label">
+										<label htmlFor="">Location : </label>
+									</div>
+									<div className="form_input_group_horizontal">
+										<div className="form_input">
+											<input id="" name="" className="" placeholder="Latitude" />
+											<div className="input_icon">
+												<FaMapMarkerAlt size="1em" />
+											</div>
+										</div>
+										<div className="form_input">
+											<input id="" name="" className="" placeholder="Longitude" />
+											<div className="input_icon">
+												<FaMapMarkerAlt size="1em" />
+											</div>
+										</div>
+									</div>
 								</div>
 							</div>
 						</div>
-
-						<div className="form_group">
-							<div className="form_label">
-								<label htmlFor="">Sealed: </label>
-							</div>
-							<div className="form_select">
-								<select name="" id="">
-									<option value="-1" className="FirstOption">--Select--</option>
-									<option value="Yes">Yes</option>
-									<option value="No">No</option>
-								</select>
-								<div className="input_icon">
-									<BsShieldLockFill size="1em" />
+						<div class="warehouse-personnel">
+							<h5>
+								Warehouse Personnel Details
+							</h5>
+							<div className="input_group three-column-grid">
+								<div className="form_group" style={{gridArea: "1 / 1 / 2 / 4"}}>
+									<div className="form_radio">
+										<label htmlFor="double_lock_yes">Double Lock System: </label>
+										<label htmlFor="double_lock_yes">Yes </label>
+										<input type={"radio"} name="double_lock" id="double_lock_yes" value="1" defaultChecked={true} onChange={(e) => {
+											console.log("Yes")
+											// console.log(e.target.checked)
+											setDoubleLockSystem(true)
+										}} />
+										<label htmlFor="double_lock_no">No </label>
+										<input type={"radio"} name="double_lock" id="double_lock_no" value="0" onChange={(e) => {
+											console.log("No")
+											// console.log(e.target.checked)
+											setDoubleLockSystem(false)
+										}} />
+									</div>
 								</div>
-							</div>
-
-						</div>
-
-						<div className="form_group">
-							<div className="form_label">
-								<label htmlFor="">Address : </label>
-							</div>
-							<div className="form_input">
-								<input id="" name="" className="" placeholder="Warehouse Address" />
-								<div className="input_icon">
-									<FaMapMarkedAlt size="1em" />
+								<div className="form_group">
+									<div className="form_label">
+										<label htmlFor="">Person Name: </label>
+									</div>
+									<div className="form_input">
+										<input placeholder="Person 1 Name" id="" name="" />
+										<div className="input_icon">
+											<BsFillPersonFill size="1em" />
+										</div>
+									</div>
 								</div>
-							</div>
 
-						</div>
-
-						<div className="form_group">
-							<div className="form_label">
-								<label htmlFor="">Warehouse Code : </label>
-							</div>
-							<div className="form_input">
-								<input id="" name="" className="" placeholder="Warehouse Code" />
-								<div className="input_icon">
-									<FaLaptopHouse size="1em" />
+								<div className="form_group">
+									<div className="form_label">
+										<label htmlFor="">Person Mobile: </label>
+									</div>
+									<div className="form_input">
+										<input type={"tel"} placeholder="Mobile" id="" name="" />
+										<div className="input_icon">
+											<BsFillTelephoneFill size="1em" />
+										</div>
+									</div>
 								</div>
-							</div>
-						</div>
 
-						<div className="form_group">
-							<div className="form_label">
-								<label htmlFor="">Location : </label>
-							</div>
-							<div className="form_input">
-								<input id="" name="" className="" placeholder="Warehouse Location (Lat, Long)" />
-								<div className="input_icon">
-									<FaMapMarkerAlt size="1em" />
+								<div className="form_group">
+									<div className="form_label">
+										<label htmlFor="">Person Designation: </label>
+									</div>
+									<div className="form_select">
+										<select name="" id="">
+											<option value="-1" className="FirstOption">--Select--</option>
+											<option value="Yes">Yes</option>
+											<option value="No">No</option>
+										</select>
+										<div className="input_icon">
+											<MdWork size="1em" />
+										</div>
+									</div>
 								</div>
-							</div>
-						</div>
 
-						<div className="form_group">
-							<div className="form_label">
-								<label htmlFor="">Double Lock System : </label>
-							</div>
-							<div className="form_select">
-								<select name="" id="">
-									<option value="-1" className="FirstOption">--Select--</option>
-									<option value="Yes">Yes</option>
-									<option value="No">No</option>
-								</select>
-								<div className="input_icon">
-									<FaLock size="1em" />
+								<div className="form_group" hidden={!doubleLockSystem}>
+									<div className="form_label">
+										<label htmlFor="">Person Name: </label>
+									</div>
+									<div className="form_input">
+										<input placeholder="Person 2 Name" id="" name="" />
+										<div className="input_icon">
+											<BsFillPersonFill size="1em" />
+										</div>
+									</div>
 								</div>
+
+								<div className="form_group" hidden={!doubleLockSystem}>
+									<div className="form_label">
+										<label htmlFor="">Person Mobile: </label>
+									</div>
+									<div className="form_input">
+										<input type={"tel"} placeholder="Mobile" id="" name="" />
+										<div className="input_icon">
+											<BsFillTelephoneFill size="1em" />
+										</div>
+									</div>
+								</div>
+
+								<div className="form_group" hidden={!doubleLockSystem}>
+									<div className="form_label">
+										<label htmlFor="">Person Designation: </label>
+									</div>
+									<div className="form_select">
+										<select name="" id="">
+											<option value="-1" className="FirstOption">--Select--</option>
+											<option value="Yes">Yes</option>
+											<option value="No">No</option>
+										</select>
+										<div className="input_icon">
+											<MdWork size="1em" />
+										</div>
+									</div>
+								</div>
+
 							</div>
 						</div>
 					</div>
-
-					<h5>
-						Warehouse Personnel Details
-					</h5>
-					<div className="input_group three-column-grid">
-
-						<div className="form_group">
-							<div className="form_label">
-								<label htmlFor="">Person Name: </label>
-							</div>
-							<div className="form_input">
-								<input placeholder="Person 1 Name" id="" name="" />
-								<div className="input_icon">
-									<BsFillPersonFill size="1em" />
-								</div>
-							</div>
-						</div>
-
-						<div className="form_group">
-							<div className="form_label">
-								<label htmlFor="">Person Mobile: </label>
-							</div>
-							<div className="form_input">
-								<input type={"tel"} placeholder="Mobile" id="" name="" />
-								<div className="input_icon">
-									<BsFillTelephoneFill size="1em" />
-								</div>
-							</div>
-						</div>
-
-						<div className="form_group">
-							<div className="form_label">
-								<label htmlFor="">Person Designation: </label>
-							</div>
-							<div className="form_select">
-								<select name="" id="">
-									<option value="-1" className="FirstOption">--Select--</option>
-									<option value="Yes">Yes</option>
-									<option value="No">No</option>
-								</select>
-								<div className="input_icon">
-									<MdWork size="1em" />
-								</div>
-							</div>
-						</div>
-
-						<div className="form_group">
-							<div className="form_label">
-								<label htmlFor="">Person Name: </label>
-							</div>
-							<div className="form_input">
-								<input placeholder="Person 2 Name" id="" name="" />
-								<div className="input_icon">
-									<BsFillPersonFill size="1em" />
-								</div>
-							</div>
-						</div>
-
-						<div className="form_group">
-							<div className="form_label">
-								<label htmlFor="">Person Mobile: </label>
-							</div>
-							<div className="form_input">
-								<input type={"tel"} placeholder="Mobile" id="" name="" />
-								<div className="input_icon">
-									<BsFillTelephoneFill size="1em" />
-								</div>
-							</div>
-						</div>
-
-						<div className="form_group">
-							<div className="form_label">
-								<label htmlFor="">Person Designation: </label>
-							</div>
-							<div className="form_select">
-								<select name="" id="">
-									<option value="-1" className="FirstOption">--Select--</option>
-									<option value="Yes">Yes</option>
-									<option value="No">No</option>
-								</select>
-								<div className="input_icon">
-									<MdWork size="1em" />
-								</div>
-							</div>
-						</div>
-
-					</div>
-
 					<center>
 						<input type={"submit"} className="mySubmit">
 							{/* <BsCheck2 size="1em" style={{ fontWeight: "bolder" }} /> <span>	Submit</span> */}
