5dec0907dfb0627e425bec18f5c1cbf134d627be
anshukumar-729 <anshukumar93.40@gmail.com>
2022-09-26 13:34:52 +0530

    userlist merged

--- src/src/components/UserList.jsx
+++ src/src/components/UserList.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import { useState, useEffect } from "react";
 import './styles/UserList.css'
+import UserDetail from './UserDetail';
 
 function UserList() {
 
+  const [isDetail, setIsDetail] = useState(0);
+
   const [users, setUsers] = useState([]);
+  const [user, setUser] = useState();
+
+
+   useEffect(() => {
+     console.log(user);
+     if (user) {
+       console.log(user);
+       setIsDetail(1);
+     }
+   }, [user]);
+
+   function details(value) {
+     console.log(value);
+     setUser(value);
+   }
+
+   function close() {
+     setIsDetail(0);
+   }
+
+
   async function getUser() {
     try {
       const response = await fetch(
@@ -88,56 +112,98 @@ function UserList() {
   // console.log(users);
   return (
     <div className="">
-      <div className='table-container'>
-        <table className='table-columns-header'>
-          <thead>
-            <tr style={{
-              background: "var(--background-gray)",
-              borderRadius: "var(--border-radius)",
-            }}>
-              <th>User ID</th>
-              <th>User Name</th>
-              <th>Active Status</th>
-              <th>Modify</th>
-              <th>Activate</th>
-              <th>Deavtivate</th>
-            </tr>
-          </thead>
-          <tbody className='table-content'>
-            {users.length > 0 && users.map((value) => (
-              <tr>
-                <td className="text-black text-sm">
-                  {value[0]}
-                </td>
-                <td className="text-black text-sm">
-                  {value[2]}
-                </td>
-                <td className="text-black text-sm">
-                  {value[7] === "A" ? "Active" : "Inactive"}
-                </td>
-                <td className="text-black text-sm">
-                  <button className='modifyBtn' disabled={true}>Modify</button>
-                </td>
-                <td>
-                  <button style={value[7] == "A" ? {opacity : '0.5', cursor : 'not-allowed'} : {opacity : '1'}} className='activateBtn' id={value[0]}  onClick={(e)=> activateUser(e.target.id)} disabled={value[7] == "A" ? true : false}>Activate</button>
-                </td>
-                <td>
-                  <button style={value[7] == "I" ? {opacity : '0.5', cursor : 'not-allowed'} : {opacity : '1'}} className='deactivateBtn' id={value[0]} onClick={(e)=> deactivateUser(e.target.id)} disabled={value[7] == "I" ? true : false} >Deactivate</button>
-                </td>
-              </tr>
-            ))} 
-            </tbody>
-        </table>
-      </div>
-      <div className="w-full flex justify-end mt-2">
-        <button
-          onClick={() => {
-            window.location.pathname = "/session/usermanagement/createUser";
-          }}
-        >
-          Add User
-        </button>
-      </div>
+      {isDetail == 1 && <UserDetail detail={user} close={close} />}
+      {isDetail == 0 && (
+        <>
+          <div className="table-container">
+            <table className="table-columns-header">
+              <thead>
+                <tr
+                  style={{
+                    background: "var(--background-gray)",
+                    borderRadius: "var(--border-radius)",
+                  }}
+                >
+                  <th>Details</th>
+                  <th>User ID</th>
+                  <th>User Name</th>
+                  <th>Active Status</th>
+                  <th>Modify</th>
+                  <th>Activate</th>
+                  <th>Deavtivate</th>
+                </tr>
+              </thead>
+              <tbody className="table-content">
+                {users.length > 0 &&
+                  users.map((value) => (
+                    <tr>
+                      <td>
+                        <button
+                          className="bg-blue-500 p-2 text-white"
+                          onClick={(e) => {
+                            details(value);
+                          }}
+                        >
+                          Detail
+                        </button>
+                      </td>
+
+                      <td className="text-black text-sm">{value[0]}</td>
+                      <td className="text-black text-sm">{value[2]}</td>
+                      <td className="text-black text-sm">
+                        {value[7] === "A" ? "Active" : "Inactive"}
+                      </td>
+                      <td className="text-black text-sm">
+                        <button className="modifyBtn" disabled={true}>
+                          Modify
+                        </button>
+                      </td>
+                      <td>
+                        <button
+                          style={
+                            value[7] == "A"
+                              ? { opacity: "0.5", cursor: "not-allowed" }
+                              : { opacity: "1" }
+                          }
+                          className="activateBtn"
+                          id={value[0]}
+                          onClick={(e) => activateUser(e.target.id)}
+                          disabled={value[7] == "A" ? true : false}
+                        >
+                          Activate
+                        </button>
+                      </td>
+                      <td>
+                        <button
+                          style={
+                            value[7] == "I"
+                              ? { opacity: "0.5", cursor: "not-allowed" }
+                              : { opacity: "1" }
+                          }
+                          className="deactivateBtn"
+                          id={value[0]}
+                          onClick={(e) => deactivateUser(e.target.id)}
+                          disabled={value[7] == "I" ? true : false}
+                        >
+                          Deactivate
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="w-full flex justify-end mt-2">
+            <button
+              onClick={() => {
+                window.location.pathname = "/session/usermanagement/createUser";
+              }}
+            >
+              Add User
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
