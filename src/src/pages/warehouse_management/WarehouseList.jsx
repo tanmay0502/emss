c877caad7da054557f3785e71c6de7f451faca83
Rohit  Parab <parabrohit161@gmail.com>
2022-09-27 06:18:19 +0000

    Update WarehouseList.jsx

--- src/src/pages/warehouse_management/WarehouseList.jsx
+++ src/src/pages/warehouse_management/WarehouseList.jsx
@@ -8,7 +8,9 @@ import {FaClipboardList} from 'react-icons/fa';
 import {FaLockOpen} from 'react-icons/fa';
 import {FaLock} from 'react-icons/fa';
 import {FaWarehouse} from 'react-icons/fa';
+import {FaCaretDown} from 'react-icons/fa';
 import './css/warehouse.css'
+import { actions } from "react-table/dist/react-table.development";
 
 
 const $ = require('jquery')
@@ -44,6 +46,7 @@ export default function WarehouseList() {
                 })
 
                 const data = await response.json();
+                console.log(data);
                 MapWarehouseTypes(data["data"]);
         } catch (error) {
             console.log(error)
@@ -78,6 +81,87 @@ export default function WarehouseList() {
             console.log(error);
         }
     }
+
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+      }
+      
+      // Close the dropdown if the user clicks outside of it
+      window.onclick = function(event) {
+        if (!event.target.matches('.btn_delete')) {
+          var dropdowns = document.getElementsByClassName("dropdown-content");
+          var i;
+          for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+            }
+          }
+        }
+      }
+
+      const ActivateWarehouse = async (myId) =>{
+        if(window.confirm(`Are you sure you want to Activate Warehouse ${myId}? `)){
+                try {
+                    
+                    const response = await fetch(
+                        `http://evm.iitbhilai.ac.in:8100/warehouse/activateWarehouse/${myId}`,
+                        {
+                            method : "GET",
+                            headers: {
+                                "Content-Type": "application/json",
+                              },
+                        }
+                    )
+
+                    const status = response;
+                    if(status.status == 200){
+                      alert("Warehouse Activated Successfully");
+                      window.location.href = '/session/warehousemanagement';
+                    }
+                    else{
+                      alert("Deactivation Failed");
+                    }
+
+                } catch (error) {
+                    console.log(error);
+                }
+        }
+  }
+
+      const DectivateWarehouse = async (myId) =>{
+            if(window.confirm(`Are you sure you want to Deactivate Warehouse ${myId}? `)){
+                    try {
+                        
+                        const response = await fetch(
+                            `http://evm.iitbhilai.ac.in:8100/warehouse/deactivateWarehouse/${myId}`,
+                            {
+                                method : "GET",
+                                headers: {
+                                    "Content-Type": "application/json",
+                                  },
+                            }
+                        )
+
+                        const status = response;
+                        if(status.status == 200){
+                          alert("Warehouse Deactivated Successfully");
+                          window.location.href = '/session/warehousemanagement';
+                        }
+                        else{
+                          alert("Deactivation Failed");
+                        }
+
+                    } catch (error) {
+                        console.log(error);
+                    }
+            }
+      }
+
+      const action = (myId,mode) => {
+          if(mode=="1") ActivateWarehouse(myId);
+          else DectivateWarehouse(myId);
+      }
     return (
         <div className="myWrapper" style={{position: "relative"}}>
             <div className='PageTitle'>
@@ -101,9 +185,10 @@ export default function WarehouseList() {
                         <th>Warehouse Type</th>
                         <th>Double Lock</th>
                         <th>Warehouse Building Type</th>
+                        <th>Active/Inactive</th>
                         <th className="btn_group">Details</th>
                         <th className="btn_group">Edit</th>
-                        <th className="btn_group">Delete</th>
+                        <th className="btn_group">Activate/Deactivate</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -121,9 +206,19 @@ export default function WarehouseList() {
                             <td>
                                <center>{value[2] === "T" ? <FaWarehouse size='1.5em' className="tempWarehouse"/> : <FaWarehouse size='1.5em' className="perWarehouse"/>}</center>
                             </td>
+                            <td>
+                                {value[13] == "A" ? "Active" : "Inactive"}
+                            </td>
                             <td><div className="btn_details" id={value[0]} onClick={()=> window.location = `warehousemanagement/warehousedetails/id=${value[0]}`}><HiDocumentDuplicate size="1em"  /><span>Details</span></div></td>
                             <td><div className="btn_edit"><FaEdit /><span>Edit</span></div></td>
-                            <td><div className="btn_delete"><FaTrashAlt /><span>Delete</span></div></td>
+                            
+                            <td>
+                                <select id={value[0]} className="mySelect" onChange={(e) => action(e.target.id,e.target.value)}>
+                                    <option value="-1">--Select--</option>
+                                    <option value='1' style={value[13] == "A" ? {display : 'none'} : {display : 'block'}} >Activate</option>
+                                    <option value='0' style={value[13] == "I" ? {display : 'none'} : {display : 'block'}} disabled={value[13] == "I" ? true : false}>Deactivate</option>
+                                </select>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
