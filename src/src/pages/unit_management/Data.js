b7c500205094c39b97cba409cb444b2b5b31f943
Ketan Mishra <ketanm@iitbhilai.ac.in>
2022-12-04 17:43:48 +0000

    Data Temp file

---
+++ src/src/pages/unit_management/Data.js
@@ -0,0 +1,203 @@
+const Data = [
+    {
+      "status": "Available for User",
+      "unit_types": [
+        {
+          "unit_type": "CU",
+          "manufacturer": "E",
+          "model": "EE",
+          "count": 2
+        }
+      ]
+    },
+    {
+      "status": "Under T&A",
+      "unit_types": [
+        {
+          "unit_type": "CU",
+          "manufacturer": "B",
+          "model": "M1",
+          "count": 1
+        },
+        {
+          "unit_type": "VT",
+          "manufacturer": "B",
+          "model": "EE",
+          "count": 1
+        },
+        {
+          "unit_type": "BU",
+          "manufacturer": "B",
+          "model": "M2",
+          "count": 3
+        },
+        {
+          "unit_type": "VT",
+          "manufacturer": "B",
+          "model": "M2",
+          "count": 1
+        },
+        {
+          "unit_type": "VT",
+          "manufacturer": "E",
+          "model": "M1",
+          "count": 1
+        },
+        {
+          "unit_type": "VT",
+          "manufacturer": "B",
+          "model": "M1",
+          "count": 1
+        }
+      ]
+    },
+    {
+      "status": "In Transit",
+      "unit_types": [
+        {
+          "unit_type": "VT",
+          "manufacturer": "B",
+          "model": "M1",
+          "count": 1
+        },
+        {
+          "unit_type": "CU",
+          "manufacturer": "E",
+          "model": "M1",
+          "count": 1
+        },
+        {
+          "unit_type": "CU",
+          "manufacturer": "E",
+          "model": "EE",
+          "count": 1
+        },
+        {
+          "unit_type": "BU",
+          "manufacturer": "E",
+          "model": "M1",
+          "count": 2
+        },
+        {
+          "unit_type": "VT",
+          "manufacturer": "E",
+          "model": "M1",
+          "count": 1
+        },
+        {
+          "unit_type": "BU",
+          "manufacturer": "B",
+          "model": "M2",
+          "count": 1
+        },
+        {
+          "unit_type": "BU",
+          "manufacturer": "B",
+          "model": "M1",
+          "count": 1
+        }
+      ]
+    },
+    {
+      "status": "FLC Assembly",
+      "unit_types": [
+        {
+          "unit_type": "BU",
+          "manufacturer": "E",
+          "model": "M1",
+          "count": 1
+        },
+        {
+          "unit_type": "BU",
+          "manufacturer": "B",
+          "model": "M2",
+          "count": 1
+        },
+        {
+          "unit_type": "CU",
+          "manufacturer": "E",
+          "model": "M2",
+          "count": 1
+        },
+        {
+          "unit_type": "BU",
+          "manufacturer": "E",
+          "model": "EE",
+          "count": 1
+        },
+        {
+          "unit_type": "CU",
+          "manufacturer": "E",
+          "model": "M1",
+          "count": 1
+        },
+        {
+          "unit_type": "BU",
+          "manufacturer": "B",
+          "model": "M1",
+          "count": 1
+        },
+        {
+          "unit_type": "BU",
+          "manufacturer": "E",
+          "model": "M2",
+          "count": 1
+        },
+        {
+          "unit_type": "CU",
+          "manufacturer": "B",
+          "model": "M2",
+          "count": 1
+        }
+      ]
+    },
+    {
+      "status": "Available for Use",
+      "unit_types": [
+        {
+          "unit_type": "BU",
+          "manufacturer": "B",
+          "model": "M1",
+          "count": 1
+        },
+        {
+          "unit_type": "BU",
+          "manufacturer": "E",
+          "model": "M1",
+          "count": 1
+        },
+        {
+          "unit_type": "VT",
+          "manufacturer": "B",
+          "model": "M1",
+          "count": 1
+        },
+        {
+          "unit_type": "VT",
+          "manufacturer": "E",
+          "model": "M1",
+          "count": 1
+        },
+        {
+          "unit_type": "CU",
+          "manufacturer": "B",
+          "model": "M1",
+          "count": 2
+        },
+        {
+          "unit_type": "VT",
+          "manufacturer": "E",
+          "model": "M2",
+          "count": 1
+        },
+        {
+          "unit_type": "VT",
+          "manufacturer": "B",
+          "model": "M2",
+          "count": 1
+        }
+      ]
+    }
+  ]
+
+  export default Data;
