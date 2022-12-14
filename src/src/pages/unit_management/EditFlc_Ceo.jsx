eee8bd5f8e448783bd57e1ae37afefa7862534e9
tanmay0502 <81376006+tanmay0502@users.noreply.github.com>
2022-12-14 10:54:58 +0530

    File Viewing

--- src/src/pages/unit_management/EditFlc_Ceo.jsx
+++ src/src/pages/unit_management/EditFlc_Ceo.jsx
@@ -393,12 +393,9 @@ function EditFlc_Ceo(props) {
                         >
                             <div id="root" className=''>
                                 <div className='flex justify-center items-center'>
-                                    {console.log("This Data:")}
-                                    {/* {console.log(photoFileData["data"].slice(0,-1))} */}
-                                    {/* {console.log(fileData)} */}
-                                    {console.log("Fetched Data:- ",photoFileData["data"])}
-                                    {/* {<embed style={{ width: "600px", height: "600px", padding: "10px" }} src={fileData} />} */}
-                                    {photoFileData["data"] !== undefined && <embed type="text/html" style={{ width: "1000px", height: "800px", padding: "10px" }} src={photoFileData["data"]} />}
+                                    {console.log(photoFileName)}
+                                    {<embed style={{ width: "600px", height: "600px", padding: "10px" }} src={``} />}
+                                    {/* {<embed style={{ width: "600px", height: "600px", padding: "10px" }} src={`${photoFileData}`} />} */}
                                 </div>
                                 <button style={{ color: "white", }} onClick={closeModal}>Close</button>
                             </div>
