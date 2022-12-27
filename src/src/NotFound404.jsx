41d1ca46e39434cf935d6027bedac95fd3b9d021
Sai Raveendra Kandregula <kandregulas@iitbhilai.ac.in>
2022-12-27 10:12:59 +0530

    Created 404 for Document Not Found in Issue Request

---
+++ src/src/NotFound404.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import styles from './404.module.css'
+
+function NotFound404(props) {
+
+    console.log(404)
+
+    return (
+        <div className={styles.Issue_Resource_Not_Found}>
+            <b>
+                4<span style={{ color: 'var(--purple)' }}>0</span>4
+            </b>
+            <h1>
+                {props.resource ? <>Oops,<br />Request Resource Not Found!</> : <>Oops,<br />Page Not Found!</>}
+                <div className={styles.GoBackButtonContainer} >
+                    {
+                        props.goBack && window.history.length !== 1 ?
+                            <>
+                                <button className={styles.GoBackButton1} >Go Back</button>
+                                <button className={styles.GoBackButton2} >Go To Home Page</button>
+                            </>
+                            :
+                            <button className={styles.GoBackButton1} >Go To Home Page</button>
+                    }
+                </div>
+            </h1>
+
+        </div>
+    )
+}
+
+NotFound404.defaultProps = {
+    resource: false,
+    goBack: true
+}
+
+export default NotFound404
--- src/src/pages/issue_request_management/FetchFile.jsx
+++ src/src/pages/issue_request_management/FetchFile.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState, useRef } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import NotFound404 from '../../NotFound404';
 
 function FetchFile() {
     const [searchParams, setSearchParams] = useSearchParams();
+
+    const [fileNotFound, setFileNotFound] = useState(false);
 
     const embedRef = useRef();
 
     const filename = window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1)
     const file_header = ';headers=filename%3D';
-    console.log(filename, searchParams.get('remarkID'))
 
     const fetchDocument = async () => {
         const response = await fetch(`${process.env.REACT_APP_API_SERVER}/issue_requests/getDocument`, {
@@ -25,11 +27,16 @@ function FetchFile() {
 
         const data = await response.json()
 
-        const tmp = data['data'].split('\',)')
-        
-        // console.log(tmp[0].replace('(\'', "") + tmp[1].slice(0, -1) )
-
-        setBase64(tmp[0].replace('(\'', "") + tmp[1].slice(0, -1))
+        if(response.status === 200){
+            const tmp = data['data'].split('\',)')
+            setBase64(tmp[0].replace('(\'', "") + tmp[1].slice(0, -1))
+        }
+        else if(response.status === 404){
+            setFileNotFound(true);
+        }
+        else if(response.status === 403){
+            // Session Expired
+        }
     }
 
     useEffect(() => {
@@ -56,8 +63,12 @@ function FetchFile() {
    
 
     return (
-        <embed ref={embedRef} style={{ width: '100vw', height: '100vh' }} title={filename} >
-        </embed>
+        <>
+            {
+                fileNotFound ? <NotFound404 resource={true} /> : <embed ref={embedRef} style={{ width: '100vw', height: '100vh' }} title={filename} >
+                </embed>
+            }
+        </>
     )
 }
 
